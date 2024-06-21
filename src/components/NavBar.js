355e980e8fb86c991831053f99024cde96967284
elkyoud-mohammed <mohammdkyoud@gmail.com>
2024-06-22 00:09:43 +0200

    certifications

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -51,7 +51,7 @@ export const NavBar = () => {
             <span className="navbar-text">
               <div className="social-icon">
                 <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
+                <a href="https://github.com/elkyoud-mohammed"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
